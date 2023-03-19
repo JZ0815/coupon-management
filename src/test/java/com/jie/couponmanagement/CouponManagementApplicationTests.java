5a9c37a7b6df47d70b5bee954364c62be138d2cb
just coding <jzhu161819@gmail.com>
2023-03-19 10:54:54 -0400

    first commit

---
+++ src/test/java/com/jie/couponmanagement/CouponManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jie.couponmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
